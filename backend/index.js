1c7a11ea594236d37cccf05fea7704e8a5015336
Dziekhey <dakeysandra@gmail.com>
2024-03-18 17:12:13 +0000

    starting with backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from"express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import skills from "./routes/skills.route.js"
+
+
+const PORT = process.env.PORT || 4000;
+
+const app = express()
+
+//add middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Load routes
+app.use('/skills', skills);
+
+// Start the server
+app.listen(PORT,() => {
+    console.log(`Server is running on port : ${PORT}`)
+})
