14134ee075e3dbb77f66b73c26695268ab9623f3
Dziekhey <dakeysandra@gmail.com>
2024-03-19 15:30:15 +0000

    other sections

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from"express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import skills from "./routes/skills.route.js"
+import projects from './routes/projects.route.js'
+import experiences from './routes/experiences.route.js'
+import achievements from './routes/achievements.route.js'
+import blogs from './routes/blogs.route.js'
 
 
 const PORT = process.env.PORT || 4000;
@@ -15,6 +19,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // Load routes
 app.use('/skills', skills);
+app.use('/projects', projects);
+app.use('/experiences', experiences);
+app.use('/achievements', achievements);
+app.use('/blogs', blogs);
 
 // Start the server
 app.listen(PORT,() => {
